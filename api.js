90de8277427d69338f47c86bb1e11827e38d8c8f
Peter Bedor <peter@lewiscommunications.com>
2020-03-30 01:53:49 -0500

    Use .env API key

--- api.js
+++ api.js
@@ -1,10 +1,7 @@
 const axios = require('axios');
-
-const API_KEY = '';
+const { API_KEY: access_token } = process.env;
 
 module.exports = axios.create({
     baseURL: 'https://api.wistia.com/v1/',
-    params: {
-        access_token: API_KEY,
-    },
+    params: { access_token },
 });
